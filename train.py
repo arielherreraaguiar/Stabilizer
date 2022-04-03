cbfdc8103f71419704cd2bec6a29104103dd4be6
Ariel Herrera <63397122+arielherreraaguiar@users.noreply.github.com>
2022-04-03 11:56:05 -0500

    Update train.py
    Update train.py

--- train.py
+++ train.py
@@ -16,9 +16,9 @@ from utils.dice_score import dice_loss
 from evaluate import evaluate
 from unet import UNet
 
-dir_img = Path('/home/inge/Pytorch-UNet/data/imgs/minidataset/')
+dir_img = Path('C:/Users/User/Desktop/Pytorch-UNet-windows/data/imgs')
 #print(dir_img)
-dir_mask = Path('/home/inge/Pytorch-UNet/data/masks/skyfinder_masks')
+dir_mask = Path('C:/Users/User/Desktop/Pytorch-UNet-windows/data/masks')
 dir_checkpoint = Path('./checkpoints/')
 
 
@@ -147,18 +147,16 @@ def train_net(net,
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
-    #parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=50, help='Number of epochs')
-    #parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=32, help='Batch size')
-    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=0.00001,
+    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
+    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
+    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
-    #parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
-    parser.add_argument('--scale', '-s', type=float, default=0.25, help='Downscaling factor of the images')
+    parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
     parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
                         help='Percent of the data that is used as validation (0-100)')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
+    parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
 
     return parser.parse_args()
 
